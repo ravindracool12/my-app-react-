8b5e40a73422c85df8e92ba7a15cea479df0c895
Ravindra Yadav <ravindry11@gmail.com>
2019-07-07 17:03:53 +0530

    new

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import Hello from './components/Hello'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponents'
+
 function App() {
   return (
     <div className="App">
-    <FunctionClick />
-    <ClassClick />
+    <ParentComponent />
+
+    {/* <EventBind /> */}
+    {/*<FunctionClick />
+        <ClassClick /> */}
       {/* <Welcome name="Bruce" heroName="Batman" /> */}
      {/* <Hello /> */}
       {/* <Counter />
